0a63c6efeee1cc5870032d4cde2b7d99d2f97127
imatmati <ivan.matmati@laposte.net>
2017-02-12 21:47:07 +0100

    suite

--- src/app/views/opendata/Opendata.js
+++ src/app/views/opendata/Opendata.js
@@ -1,27 +1,78 @@
 import React , {Component} from 'react'
-import Page from './Page'
+import Griddle from 'griddle-react'
 import {
-    fetchAliments
+    Typeahead
+} from 'react-bootstrap-typeahead';
+import {
+    fetchAlimentsNames,
+    fetchAlimentById,calculateCalorie
 }
 from "../../services"
+import shallowCompare from 'react-addons-shallow-compare';
+import {Jumbotron}      from '../../components';
 
 export default class Opendata extends Component {
 
-  state = {
-    aliments : []
-  }
-  componentDidMount() {
-    fetchAliments().then(aliments => {
+  columns = ["ORIGFDNM","proteines", "glucides", "lipides"];
+  columnsNames = [{"columnName": "ORIGFDNM", "displayName": "Nom"},{"columnName": "proteines", "displayName": "Protéines"},
+                  {"columnName": "glucides", "displayName": "Glucides"}, {"columnName": "lipides", "displayName": "Lipides"},
+                  {"columnName": "calories", "displayName": "Calories"}];
+  columnsWithCalories =  this.columns.concat(["calories"])
+
+    componentDidMount() {
+        fetchAlimentsNames().then(data => {
             this.setState({
-                aliments
-
+                alimentNames: data
             })
-        }).catch(err => {
-            console.log ("err",err)
         })
-  }
+    }
 
-  render() {
-    return(<Page results={this.state.aliments} />)
-  }
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            alimentNames: [],
+            aliment : {}
+        };
+    }
+
+    getAliment(alimentSelected) {
+        fetchAlimentById(alimentSelected[0]._id).then(data => {
+            this.setState({
+                aliment: data
+            })
+        });
+    }
+
+    calculate(evt) {
+      calculateCalorie(this.state.aliment._id).then(data=> {
+        this.setState({
+            aliment: data[0]
+        });
+
+      });
+    }
+
+    render() {
+        return (<Jumbotron>
+          <div className="row">
+              <div className="col-md-6 col-xs-12">
+                    <Typeahead options = { this.state.alimentNames } labelKey = { "ORIGFDNM" } onChange = {this.getAliment.bind(this)}  placeholder = "Nom d'aliments"/>
+              </div>
+              <div className="col-md-6">
+                {this.state.aliment && <button type="button" className="btn btn-primary" onClick={this.calculate.bind(this)}>Calcul Calories</button>}
+            </div>
+          </div>
+          <div className="row top-buffer">
+            <div className="col-md-12">
+                    <Griddle  key={this.state.aliment.calories} columnMetadata={this.columnsNames}
+                             showPager={false}  results={this.state.aliment && [this.state.aliment] || []}
+                             columns={this.state.aliment.calories && this.columnsWithCalories || this.columns}
+                             noDataMessage="Vous n'avez pas encore choisi votre aliment" />
+                </div>
+           </div>
+          </Jumbotron>
+        );
+    }
 }
